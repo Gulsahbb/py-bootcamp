a611a3e1154be2fbe097b33dca5ecc7f3ae8ef88
Gulsahbb <gulsahbaskurt.10@gmail.com>
2024-08-10 22:33:21 +0300

    updated

---
+++ 2.statements_and_comments.py
@@ -0,0 +1,26 @@
+pi = 3.14
+print(pi)
+
+# --------------------------------------
+# Yorum satırı yazarken başına # işareti getirilir.
+def selamlama (name):
+    print(f'Merhaba, {name}')
+
+selamlama("Gulsah")
+
+# --------------------------------------
+"""
+Uzun
+Yorum
+Satırı
+Yazılacağı
+Zaman
+Üç tırnak işareti
+içerisine 
+yazılır.
+"""
+pi = 3.14
+if pi < 3.15:
+  pi = pi + 0.1
+
+print(pi)
