d64cc43615d7ac83ce70dde136e58727d392655f
Gulsahbb <gulsahbaskurt.10@gmail.com>
2024-08-10 21:57:00 +0300

    deneme

--- test.py
+++ test.py
@@ -10,9 +10,17 @@ def carpma(sayi1,sayi2):
 def bolme(sayi1,sayi2):
     return sayi1 / sayi2
 
-sonuc = toplama(4,3)
-sonuc = cikarma(19,3)
-sonuc = carpma(7,234)
-sonuc = bolme(45,6)
-print(sonuc)
+# sonuc = toplama(4,3)
+# sonuc = cikarma(19,3)
+# sonuc = carpma(7,234)
+# sonuc = bolme(45,6)
+# print(sonuc)
+
+sayi1 = int(input('Lütfen 1.sayıyı Giriniz. : '))
+sayi2 = int(input('Lütfen 2.sayıyı Giriniz. : '))
+
+print('Toplam : ',toplama(sayi1,sayi2))
+print('Fark : ',cikarma(sayi1,sayi2))
+print('Çarpım : ',carpma(sayi1,sayi2))
+print('Bölüm : ',bolme(sayi1,sayi2))
 
