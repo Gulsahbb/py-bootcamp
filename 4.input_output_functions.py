f4c506b6188e0e35aab8090a5c5b5bff0d25662d
Gulsahbb <gulsahbaskurt.10@gmail.com>
2024-08-10 22:56:40 +0300

    deneme

---
+++ 4.input_output_functions.py
@@ -0,0 +1,31 @@
+print("Bu bir Python Dosyası")
+
+isim = "Ayşe"
+print(isim)
+
+isim = "Ali"
+print(f'Merhaba, {isim}')
+
+# isim = input("İsim Giriniz : ")
+# print(f'Hoşgeldin {isim}')
+
+sayi = 14
+print(sayi)
+
+sayi = input("Sayı Giriniz. :")
+print(f'Girilen Sayı : {sayi}')
+"""
+HATA!!
+input ile istenilen değer string olarak döner. Bu yüzden integer bir değer ile toplama yapamıyoruz.
+
+toplam = sayi + 10
+print(f'Sayı Toplamı : {toplam}')
+
+O yüzden 
+
+sayi = int(input("Sayı Giriniz. :"))
+print(f'Girilen Sayı : {sayi}')
+
+şeklinde yazabiliriz.
+"""
+
