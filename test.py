6fce25b951323c291362063d690ccf9b6c33fef2
Gulsahbb <gulsahbaskurt.10@gmail.com>
2024-08-10 21:41:18 +0300

    deneme

---
+++ test.py
@@ -0,0 +1,16 @@
+def toplama(sayi1,sayi2):
+    return sayi1 + sayi2
+
+def cikarma(sayi1,sayi2):
+    return sayi1 - sayi2
+
+def carpma(sayi1,sayi2):
+    return sayi1 * sayi2
+
+def bolme(sayi1,sayi2):
+    return sayi1 / sayi2
+
+sonuc = toplama(4,3)
+sonuc = cikarma(19,3)
+print(sonuc)
+
