a12c8428dedde217449c06393afd57f11fa154ad
Gulsahbb <gulsahbaskurt.10@gmail.com>
2024-08-10 23:05:48 +0300

    deneme

---
+++ 5.import.py
@@ -0,0 +1,19 @@
+"""
+import math
+
+sayi = 15.85698
+
+print(math.floor(sayi))
+print(math.ceil(sayi))
+print(math.exp(sayi))
+
+import random
+
+sayi = random.randint(0,10)
+print(f'Rastgele Sayı Üretiliyor ... Sayı : {sayi}')
+"""
+import datetime
+tarih = datetime.datetime.now()
+print(tarih)
+
+
