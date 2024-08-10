ea44773e7a485bf458bf49e66eae6353cf542582
Gulsahbb <gulsahbaskurt.10@gmail.com>
2024-08-10 23:09:00 +0300

    deneme

---
+++ 6.operators.py
@@ -0,0 +1,51 @@
+print(2 + 5)
+
+# OPERATÖRLER
+print("5 + 2 =", 5+2)
+print("5 - 2 =", 5-2)
+print("5 * 2 =", 5*2)
+print("5 / 2 =", 5/2)
+print("5 // 2 =", 5//2)
+print("5 ** 2 =", 5**2)
+print("5 % 2 =", 5%2)
+
+"""
+x = 5 # equivalent to x = 5
+x += 5 # equivalent to x = x + 5
+x -= 5 # equivalent to x = x - 5
+x *= 5 # equivalent to x = x * 5
+x /= 5 # equivalent to x = x / 5
+x %= 5 # equivalent to x = x % 5
+x //= 5 # equivalent to x = x // 5
+x **= 5 # equivalent to x = x ** 5
+"""
+
+# TRUE _ FALSE
+num1 = 10
+num2 = 5
+
+print(num1, ">", num2, "=", num1 > num2)
+print(num1, ">=", num2, "=", num1 >= num2)
+print(num1, "<", num2, "=", num1 < num2)
+print(num1, "<=", num2, "=", num1 <= num2)
+print(num1, "==", num2, "=", num1 == num2)
+print(num1, "!=", num2, "=", num1 != num2)
+
+# VE OPERATÖRÜ
+print("True and True =", True and True)
+print("True and False =", True and False)
+print("False and True =", False and True)
+print("False and False =", False and False)
+
+print(True and False and False)
+
+# VEYA OPERATÖRÜ
+
+print("True or True =", True or True)
+print("True or False =", True or False)
+print("False or True =", False or True)
+print("False or False =", False or False)
+
+# DEĞİL OPERATÖRÜ
+print("not True =", not True)
+print("not False =", not False)
